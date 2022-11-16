ed61e4d316a5660aa0903b596d43c6de8ce0dd1e
Mosatfa Ahmed <mostafa.ahmed19293949@gmail.com>
2022-11-16 20:23:51 +0200

    update

---
+++ Day1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "LinkedList.h"
+
+int main()
+{
+    /*LinkedList myList = {.head = NULL, .tail = NULL};
+    Add(&myList, 3);*/
+
+    Add(9);
+    Add(4);
+    Add(2);
+    Add(1);
+
+    Display();
+
+    printf("\n%d",GetCount());
+
+    GetDataByIndex(3);
+
+    printf("\n");
+
+    InsertAfter(10, 4);
+
+    Display();
+
+    return 0;
+}
